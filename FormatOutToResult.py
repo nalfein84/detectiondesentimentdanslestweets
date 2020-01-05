84498aa047740331f70135aa473da03fac93f062
Passemard Kévin <passemard.kevin@hotmail.fr>
2020-01-05 11:58:07 +0100

    Suppression du fichier FormatOutToResult
    Suppression du fichier FormatOutToResult (remplacé par la fonction PredictionSVMToOut dans projectHelper)

--- FormatOutToResult.py
+++
@@ -1,6 +0,0 @@
-from projectHelper import txtFileToListe
-from projectHelper import LabelConverter
-from projectHelper import WriteInFile
-from projectHelper import PredictionSVMToOut
-
-PredictionSVMToOut("GenerationFichier/resultModel.txt")
