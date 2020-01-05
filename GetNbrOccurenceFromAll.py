f5dfeac8f3a3e2afb95fe6fa4e3f50a89e718c1c
Passemard Kévin <passemard.kevin@hotmail.fr>
2020-01-05 12:00:00 +0100

    Delete GetNbrOccurenceFromAll.py
    Suppression du Fichier GetNbrOccurenceFromAll.py (non utilisé par les autres scriptes python)

--- GetNbrOccurenceFromAll.py
+++
@@ -1,8 +0,0 @@
-from projectHelper import getIndexFromDonneeTest
-from projectHelper import getIndexFromTP
-from projectHelper import getIndexFromTweet
-from IndexBuilder import IndexBuilder
-import re
-
-index = getIndexFromDonneeTest(getIndexFromTP(getIndexFromTweet()))
-index.SaveNbrOccurence(salt="allWord_")
